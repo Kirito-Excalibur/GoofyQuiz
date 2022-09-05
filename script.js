0e7a0607f250bff995462f33cd7a0d3da41a98a6
Nikhil <nikhilsimontoppo@gmail.com>
2022-09-05 22:21:08 +0530

    added main menu

--- script.js
+++ script.js
@@ -32,16 +32,16 @@ const quizData = [
     correct: "a",
   },
   {
-    question: "What is the most famous identity of Nikhil Toppo?",
-    a: "Chotu",
-    b: "Self declared genius",
-    c: "Walking Talking Dictionary",
-    d: "Ms.Soumika's secretary",
+    question: "What is Goofy's dream company to work in?",
+    a: "Google",
+    b: "Microsoft",
+    c: "Amazon",
+    d: "Kidney Corp.",
     correct: "d",
   },
 ];
 
-const quiz=document.getElementById("contain")
+const quiz = document.getElementById("contain")
 const answersEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
@@ -78,7 +78,7 @@ function getSelected() {
 
 function deselectAnswers() {
   answersEls.forEach((answerEl) => {
-    answerEl.checked=false;
+    answerEl.checked = false;
   });
 }
 
@@ -94,7 +94,7 @@ sumbitBtn.addEventListener("click", () => {
     if (currentQuiz < quizData.length) {
       loadQuiz();
     } else {
-      quiz.innerHTML=`<h2>You answered correctly at ${score}/${quizData.length} questions. </h2>`
+      quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions. </h2>`
     }
   }
 });
